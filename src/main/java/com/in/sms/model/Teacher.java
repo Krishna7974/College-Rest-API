daa925dde685c833cc1d3c0ae67a419ca6a0945d
Shrkrishna Patidar <krishnapatidar7974@gmail.com>
2025-07-02 19:26:38 +0530

    liquibase updated

--- src/main/java/com/in/sms/model/Teacher.java
+++ src/main/java/com/in/sms/model/Teacher.java
@@ -20,8 +20,10 @@ public class Teacher {
 
     @Column(nullable = false,unique = true)
     private String name;
+
     @Column(nullable = false,unique = true)
     private String email;
+
     @Column(nullable = false)
     private String password;
 
